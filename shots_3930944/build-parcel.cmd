0dc481e04c12c6579e7ff49dde6b403a06cabd51
Pavel Timofeev <pavel.timofeev@hotmail.com>
2018-07-09 23:41:50 +0300

    fixed navbar...

--- shots_3930944/build-parcel.cmd
+++ shots_3930944/build-parcel.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 rmdir release /S /Q
-parcel build  --no-cache --no-source-maps -d release index.html
+parcel build  --no-cache --no-source-maps --no-minify -d release index.html
 
 pause
