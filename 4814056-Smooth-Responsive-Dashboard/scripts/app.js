1b5ba62af0d5aa81c336567d0499984725f3fb75
Pavel Timofeev <pavel.timofeev@hotmail.com>
2018-07-23 12:10:25 +0300

    one/two-column left side with hiding chart; cleaning layout; added widgets

--- 4814056-Smooth-Responsive-Dashboard/scripts/app.js
+++ 4814056-Smooth-Responsive-Dashboard/scripts/app.js
@@ -23,14 +23,20 @@ $(document).ready(function() {
     edit_state = false;    
   }
   
-  function setMode( mode ) {
+  function setMode( mode, hide ) {
     
     var ch = $('.charts-group .chart');
     ch.removeClass('chart--'+right_mode);
     ch.addClass('chart--'+mode);
     
+    var ch_h = $('.charts-group .chart__chart');
+    ch_h.removeClass('chart__chart--hide');
+    if( hide ) {
+      ch_h.addClass('chart__chart--hide');
+    }
+    
     console.log( mode );
-    right_mode = mode;
+    right_mode = mode + (hide === true ? "--hide": "");
   }
   
   function set( x ){
@@ -38,22 +44,28 @@ $(document).ready(function() {
       var left = x;
       var right = window.innerWidth - x;
       
-      if( x > window.innerWidth * 0.3 && x < window.innerWidth * 0.8 ) {
+      var l_max = window.innerWidth * 0.5;
+      var l_min = window.innerWidth * 0.75;
+      
+      var l_treshold_one = window.innerWidth * 0.6;
+      var l_treshold_two = window.innerWidth * 0.7;
+      
+      if( x > l_max && x < l_min ) {
         
         left_side.width( left );
         right_side.width( right );        
       
-        if( right < 400 && right_mode !== "one" )
+        if( left < l_treshold_one && right_mode !== "two" )
         {
-          setMode( "one" );
+          setMode( "two", false );
         }
-        else if ( right > 400 && right < 600  && right_mode !== "two")
+        else if ( left > l_treshold_one && left < l_treshold_two && right_mode !== "one")
         {
-          setMode( "two" );
+          setMode( "one", false );
         }
-        else if(  right > 600 && right_mode !== "three")
+        else if(  left > l_treshold_two && right_mode !== "one--hide")
         {
-           setMode( "three" );
+          setMode( "one", true );
         }
       }
   }
